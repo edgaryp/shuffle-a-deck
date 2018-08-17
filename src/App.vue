373fdf275473ecda9455084474e1449c2fb74011
Edgar Pan <edgarpan@Edgars-MBP.gateway>
2018-08-17 23:27:25 +1000

    added shuffle feature

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,220 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1 class="title">
+      <img class="vue-logo" src="./assets/logo.png" />
+      Card Shuffling
+    </h1>
+    <div class="speed-buttons">
+      <button v-for="(type, index) in shuffleTypes" class="button is-small" :key="index" :class="{ 'is-light': shuffleSpeed != `shuffle${type}` }" @click="shuffleSpeed = `shuffle${type}`">
+        {{ type }}
+      </button>
+    </div>
+    <div class="main-buttons">
+      <button v-if="isDeckShuffled" @click="displayInitialDeck" class="button is-primary is-outlined">
+        Reset <i class="fas fa-undo"></i>
+      </button>
+      <button @click="shuffleDeck" class="button is-primary">
+        Shuffle <i class="fas fa-random"></i>
+      </button>
+    </div>
+    <transition-group :name="shuffleSpeed" tag="div" class="deck">
+      <decks v-for="(card, index) in this.cards" :key="index" :card="card" :suitColor="suitColor"></decks>
+    </transition-group>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Decks from './components/decks.vue';
+import Vue from 'vue';
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    Decks
+  },
+  data () {
+    return {
+      ranks: ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'],
+      suits: ['♥', '♦', '♠', '♣'],
+      cards: [],
+      suitColor: {
+        '♠': 'black',
+        '♣': 'black',
+        '♦': 'red',
+        '♥': 'red'
+      },
+      shuffleSpeed: 'shuffleMedium',
+      shuffleTypes: ['Slow', 'Medium', 'Fast'],
+      isDeckShuffled: false,
+      shuffleCount: 0
+    };
+  },
+  methods: {
+    displayInitialDeck () {
+      let id = 1;
+      this.cards = [];
+      let card = {};
+      this.suits.forEach(suit => {
+        this.ranks.forEach(rank => {
+          card = {
+            id: id,
+            rank: rank,
+            suit: suit
+          };
+          this.cards.push(card);
+        });
+        id++;
+      });
+    },
+    shuffleDeck () {
+      let i = this.cards.length;
+      let randomNumber;
+      let temp;
+      while (--i > 0) {
+        randomNumber = Math.floor(Math.random() * i);
+        temp = this.cards[i];
+        Vue.set(this.cards, i, this.cards[randomNumber]);
+        Vue.set(this.cards, randomNumber, temp);
+      }
+    }
+  },
+  created () {
+    this.displayInitialDeck();
   }
-}
+};
 </script>
 
 <style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+html, body, #app {
+  height: 100%;
+  background: ghostwhite;
+}
+
+.title {
+  font-family: Roboto Slab, sans-serif;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding-top: 30px;
+  margin-bottom: 0 !important;
+  font-weight: 300;
+  font-size: 3rem;
+}
+
+.vue-logo {
+  height: 55px;
+  position: relative;
+  top: 10px;
+}
+
+.speed-buttons {
+  text-align: center;
+  padding-top: 30px;
+}
+.speed-buttons .button {
+  height: 2.50em;
+}
+
+.main-buttons {
+  display: block;
+  margin: 0 auto;
+  text-align: center;
+  padding-top: 30px;
+  margin-bottom: 0 !important;
+}
+
+.count-section {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+}
+
+.fas {
+  padding-left: 5px;
+}
+
+.deck {
+  margin-left: 30px;
+  padding-top: 30px;
+}
+
+.card {
+  width: 75px;
+  height: 100px;
+  float: left;
+  margin-right: 5px;
+  margin-bottom: 5px;
+  border-radius: 2px;
+}
+
+.card__suit {
+  width: 100%;
+  display: block;
+}
+
+.card__suit--top {
+  text-align: left;
+  padding-left: 5px;
+}
+
+.card__suit--bottom {
+  position: absolute;
+  bottom: 0px;
+  text-align: left;
+  transform: rotate(180deg);
+  padding-left: 5px;
+}
+
+.card__number {
+  width: 100%;
+  position: absolute;
+  top: 38%;
+  text-align: center;
+}
+
+.red {
+  color: #FF0000;
+}
+
+.black {
+  color: #000;
+}
+
+.twitter-section {
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+}
+.twitter-section .fa-twitter-square {
+  color: #00d1b2;
+  font-size: 30px;
+}
+
+.shuffleSlow-move {
+  transition: transform 2s;
+}
+
+.shuffleMedium-move {
+  transition: transform 1s;
+}
+
+.shuffleFast-move {
+  transition: transform 0.5s;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+
+@media (max-width: 1210px) {
+  .deck {
+    position: initial;
+    top: 0;
+  }
+
+  .twitter-section {
+    display: none;
+  }
 }
 </style>
---
+++ src/components/decks.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="card" :class="suitColor[card.suit]" :key="index">
+      <span class="card__suit card__suit--top">{{ card.suit }}</span>
+      <span class="card__number">{{ card.rank }} </span>
+      <span class="card__suit card__suit--bottom">{{ card.suit }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'card',
+    'suitColor',
+    'index'
+  ]
+};
+</script>
+
+<style lang="less" scoped>
+</style>
